6ad2ca32792328619089d4cea0ecb65521768728
devkush0999 <devesh2cse@gmail.com>
2025-02-01 20:47:38 +0530

    basic setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Navigation from '@navigation/Navigation'
+
+
+const App = () => {
+  return (
+    <Navigation/>
+  )
+}
+
+export default App
