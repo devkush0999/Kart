7585a8b6afd126fffab421715f291b20eef8063d
devkush0999 <devesh2cse@gmail.com>
2025-02-01 20:43:21 +0530

    setup

---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * @format
+ */
+
+import {AppRegistry, TextInput} from 'react-native';
+import App from './src/App';
+import {name as appName} from './app.json';
+import { configureReanimatedLogger, ReanimatedLogLevel } from 'react-native-reanimated'
+configureReanimatedLogger({
+    level: ReanimatedLogLevel.warn,
+    strict: false,
+});
+
+if(Text.defaultProps){
+    Text.defaultProps.allowFontScaling = false;
+}else{
+    Text.defaultProps={}
+    Text.defaultProps.allowFontScaling = false;
+}
+
+if(TextInput.defaultProps){
+    TextInput.defaultProps.allowFontScaling = false;
+}else{
+    TextInput.defaultProps={}
+    TextInput.defaultProps.allowFontScaling = false;
+}
+
+AppRegistry.registerComponent(appName, () => App);
