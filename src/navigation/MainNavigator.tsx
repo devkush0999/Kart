2a963d125f8b9d4aff2ada2885a905625bd8bb88
devkush0999 <devesh2cse@gmail.com>
2025-02-04 23:10:55 +0530

    .

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -6,11 +6,12 @@ import Home from '@modules/home';
 import Account from '@modules/account';
 import Category from '@modules/category';
 import Cart from '@modules/cart';
-import { HomeIcon } from './TabIcons';
+import { AccountIcon, CategoryIcon, HomeIcon } from './TabIcons';
 
 const Tab = createBottomTabNavigator();
 
 const MainNavigator:FC = () => {
+    const count = 2
   return (
     <Tab.Navigator screenOptions={{
         headerShown: false,
@@ -26,9 +27,19 @@ const MainNavigator:FC = () => {
         <Tab.Screen name="Home" component={Home} options={{ tabBarIcon:({focused,color,size})=>(
             <HomeIcon focused={focused} color={color} size={size} />
         )}} />
-        <Tab.Screen name="Category" component={Category} />
-        <Tab.Screen name="Account" component={Account} />
-        <Tab.Screen name='Cart' component={Cart} />
+        <Tab.Screen name="Category" component={Category} options={{ tabBarIcon:({focused,color,size})=>(
+            <CategoryIcon focused={focused} color={color} size={size} />
+        )}} />
+       
+        <Tab.Screen name='Cart' component={Cart} options={{ tabBarIcon:({focused,color,size})=>(
+            <CategoryIcon focused={focused} color={color} size={size} />
+        ), tabBarBadge:count >0? count:undefined, tabBarBadgeStyle:{
+            height:16,
+            width:16
+        }}} />
+         <Tab.Screen name="Account" component={Account} options={{ tabBarIcon:({focused,color,size})=>(
+            <AccountIcon focused={focused} color={color} size={size} />
+        )}} />
     </Tab.Navigator>
   )
 }
